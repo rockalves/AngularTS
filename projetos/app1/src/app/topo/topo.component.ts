9162328dcf27bd3bc40db57a4f7afa17b74fd378
rockalves <andreralves@hotmail.com>
2018-09-23 12:01:20 -0300

    Tipos de selectors

--- projetos/app1/src/app/topo/topo.component.ts
+++ projetos/app1/src/app/topo/topo.component.ts
@@ -1,7 +1,24 @@
 import {Component} from 'node_modules/@angular/core'
 
 @Component({
-    selector:'app-topo',
+    /** 
+     * Selectors: 
+     * 
+     * selector:'.app-topo' (ponto)
+     * Será utilizado no html do componente como uma classe
+     * Ex.: <div class='app-topo'>
+     * 
+     * selector:'app-topo' (componente)
+     * SErá utilizado como componente em tag html
+     * Ex.: <app-topo></app-topo>
+     * 
+     * selector:'[app-topo]'
+     * Será utilizado em forma de atributo
+     * Ex.: <div app-topo></div> 
+     * 
+     * * */
+    
+    selector:'.app-topo',
     templateUrl:'./topo.component.html',
     styleUrls:['./topo.component.css']
     //styles:['.exemplo{color:red}']
