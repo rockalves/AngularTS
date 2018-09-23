aac83813da549d5262e57d0fe266bf7bd39c1e27
rockalves <andreralves@hotmail.com>
2018-09-23 11:50:51 -0300

    adicionado estilo css

--- projetos/app1/src/app/topo/topo.component.ts
+++ projetos/app1/src/app/topo/topo.component.ts
@@ -2,7 +2,9 @@ import {Component} from 'node_modules/@angular/core'
 
 @Component({
     selector:'app-topo',
-    templateUrl:'./topo.component.html'
+    templateUrl:'./topo.component.html',
+    styleUrls:['./topo.component.css']
+    //styles:['.exemplo{color:red}']
 }
 )
 
