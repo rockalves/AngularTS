2439cb7d87354f5535efe34b3043c5e5310fb862
rockalves <andreralves@hotmail.com>
2018-09-23 11:43:06 -0300

    Ajustes para exibicao do topo

--- projetos/app1/src/app/topo/topo.component.ts
+++ projetos/app1/src/app/topo/topo.component.ts
@@ -1,10 +1,9 @@
 import {Component} from 'node_modules/@angular/core'
-import { templateSourceUrl } from '@angular/compiler';
+
 @Component({
     selector:'app-topo',
     templateUrl:'./topo.component.html'
 }
-
 )
 
 export class TopoComponent{
