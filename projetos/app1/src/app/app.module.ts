dc98f5ff0b1d7ad077d946a33a7e558541320742
rockalves <andreralves@hotmail.com>
2018-09-24 12:53:53 -0300

    Adicionado componente painel via angular-cli

--- projetos/app1/src/app/app.module.ts
+++ projetos/app1/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from 'node_modules/@angular/platform-browser';
 import { NgModule } from 'node_modules/@angular/core';
-
 import { AppComponent } from './app.component';
-import { TopoComponent } from './topo/topo.component'
+import { TopoComponent } from './topo/topo.component';
+import {PainelComponent } from './painel/painel.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopoComponent
+    TopoComponent,
+    PainelComponent
   ],
   imports: [
     BrowserModule
--- projetos/app1/src/app/topo/topo.component.ts
+++ projetos/app1/src/app/topo/topo.component.ts
@@ -18,7 +18,7 @@ import {Component} from 'node_modules/@angular/core'
      * 
      * * */
     
-    selector:'.app-topo',
+    selector:'app-topo',
     templateUrl:'./topo.component.html',
     styleUrls:['./topo.component.css']
     //styles:['.exemplo{color:red}']
