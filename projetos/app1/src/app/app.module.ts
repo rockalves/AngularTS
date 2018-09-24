b27bb2ef55130bcfa270aba430611054753df94d
rockalves <andreralves@hotmail.com>
2018-09-24 13:44:17 -0300

    Adicionado Bootstrap no projeto. Incluio botão teste no painel

--- projetos/app1/src/app/app.module.ts
+++ projetos/app1/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from 'node_modules/@angular/core';
 import { AppComponent } from './app.component';
 import { TopoComponent } from './topo/topo.component';
 import {PainelComponent } from './painel/painel.component';
+import {TentativasComponent } from './tentativas/tentativas.component';
+import {ProgressoComponent } from './progresso/progresso.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopoComponent,
-    PainelComponent
+    PainelComponent,
+    TentativasComponent,
+    ProgressoComponent
   ],
   imports: [
     BrowserModule
