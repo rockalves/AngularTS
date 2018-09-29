5f73de093a864daf69e97b048671405a845354fe
rockalves <andreralves@hotmail.com>
2018-09-28 23:08:48 -0300

    Import no painel e logando o array de frases

--- projetos/app1/src/app/painel/frases-mock.ts
+++ projetos/app1/src/app/painel/frases-mock.ts
@@ -1,12 +1,11 @@
 import { Frase } from '../shared/frase.model'
 
-const FRASES: Frase[] = [
+export const FRASES: Frase[] = [
     { fraseEng:'I like to learn',frasePtBr:'Eu gosto de aprender'},
     { fraseEng:'I watch TV',frasePtBr:'Eu assisto TV'},
     { fraseEng:'How are you?',frasePtBr:'Como vai você?'},
     { fraseEng:'I eat bread',frasePtBr:'Eu como pão'}
 ]
 
-
-//Modo alternativo:
+//Modo de declaração alternativo:
 //const FRASES:Array<Frase>=[]
