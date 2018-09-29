ff1d53ad5843c35fbbb921dac370d3bdfae98129
rockalves <andreralves@hotmail.com>
2018-09-28 23:47:08 -0300

    Event binding na insercao de texto. Insercao de log

--- projetos/app1/src/app/painel/painel.component.ts
+++ projetos/app1/src/app/painel/painel.component.ts
@@ -10,10 +10,16 @@ import {FRASES } from './frases-mock'
 export class PainelComponent implements OnInit {
   
   public instrucao : string = 'Traduza a frase:'
-  public frases : Frase[] = FRASES
-  constructor() {console.log(this.frases) }
+  public frases : Frase[] = FRASES //Instancia do array de frases, passando como atributo o mock (enum) de frases
+
+  constructor() {console.log(this.frases) } //loga o array de frases
 
   ngOnInit() {
   }
+
+  public atualizaResposta():void{
+    console.log("<- Qtd evento input - Teste")
+  }
+
 
 }
