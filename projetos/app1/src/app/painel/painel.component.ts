bf1bb5a4a441da87cc2958d328ebbfea9a8642a2
rockalves <andreralves@hotmail.com>
2018-10-01 13:02:10 -0300

    Receber texto completo do campo de resposta, uitilizando o target

--- projetos/app1/src/app/painel/painel.component.ts
+++ projetos/app1/src/app/painel/painel.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Frase } from '../shared/frase.model'
-import {FRASES } from './frases-mock'
+import { Frase } from '../shared/frase.model'
+import { FRASES } from './frases-mock'
 
 @Component({
   selector: 'app-painel',
@@ -8,18 +8,22 @@ import {FRASES } from './frases-mock'
   styleUrls: ['./painel.component.css']
 })
 export class PainelComponent implements OnInit {
-  
-  public instrucao : string = 'Traduza a frase:'
-  public frases : Frase[] = FRASES //Instancia do array de frases, passando como atributo o mock (enum) de frases
 
-  constructor() {console.log(this.frases) } //loga o array de frases
+  public instrucao: string = 'Traduza a frase:'
+  public frases: Frase[] = FRASES //Instancia do array de frases, passando como atributo o mock (enum) de frases
+  public resposta: string
+
+  constructor() { console.log(this.frases) } //loga o array de frases
 
   ngOnInit() {
   }
 
-  public atualizaResposta(resposta:Event):void{
-    console.log(resposta);
-    console.log("<- Qtd evento input - Teste");
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+    console.log(this.resposta)
+    //console.log((<HTMLInputElement>resposta.target).value);
+    //Obtem o valor completo do input.
+    //console.log("<- Qtd evento input - Teste");
   }
 
 
