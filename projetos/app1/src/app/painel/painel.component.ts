d483f13e2dfcad8e40f6fa8246925da3e09bf80a
rockalves <andreralves@hotmail.com>
2018-10-03 12:46:34 -0300

    Atributos para controlar a frase da rodada

--- projetos/app1/src/app/painel/painel.component.ts
+++ projetos/app1/src/app/painel/painel.component.ts
@@ -13,7 +13,13 @@ export class PainelComponent implements OnInit {
   public frases: Frase[] = FRASES //Instancia do array de frases, passando como atributo o mock (enum) de frases
   public resposta: string
 
-  constructor() { console.log(this.frases) } //loga o array de frases
+  public rodada: number = 0
+  rodadaFrase: Frase
+
+  constructor() { 
+    this.rodadaFrase = this.frases[this.rodada]
+    console.log(this.rodadaFrase) 
+  } //loga o array de frases
 
   ngOnInit() {
   }
@@ -25,8 +31,8 @@ export class PainelComponent implements OnInit {
     //Obtem o valor completo do input.
 
   }
-public verificarResposta():void{
-  console.log('Verificar resposta: ', this.resposta)
-}
+  public verificarResposta(): void {
+    console.log('Verificar resposta: ', this.resposta)
+  }
 
 }
