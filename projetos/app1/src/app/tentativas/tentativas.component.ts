605dcbfd0b633fabdd8f3bbadd4d6ecb6b4382d5
rockalves <andreralves@hotmail.com>
2018-10-17 23:02:28 -0300

    ngOnChages de tentativas atualiza a imagem de coracoes, decrementando conforme as tentativas restantes

--- projetos/app1/src/app/tentativas/tentativas.component.ts
+++ projetos/app1/src/app/tentativas/tentativas.component.ts
@@ -23,6 +23,19 @@ export class TentativasComponent implements OnInit, OnChanges {
 
   }
   ngOnChanges(){
+    //this.tentativas Numero de tentativas
+    //this.coracoes.length Tamanho do array de coracoes
+    //no trecho abaixo, realizamos a comparacao entre o numero de tentativas, 
+    // com o tamanho do array de coracoes, caso seja diferente atribui o valor do indice 
+    // com a subtracao do tamanho do array de coracoes pelo numero de tentativas restantes
+    // onde o caminho da imagem sera alterado de acordo com a posicao do array de coracoes
+    // atribuida pelo indice 
+    if (this.tentativas !== this.coracoes.length){
+      let indice = this.coracoes.length - this.tentativas
+      // 3 - 2 = 1 
+      this.coracoes[indice - 1].cheio = false;
+    }
+
     console.log('tentativas recebidas do painel ngOnChanges: ',this.tentativas)
   }
 }
